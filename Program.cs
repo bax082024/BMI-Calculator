b59c8257564379c6dd102fbb59b24298c065c87a
bax082024 <bax082024@gmail.com>
2024-11-28 07:07:43 +0100

    added validnumberscheck

--- Program.cs
+++ Program.cs
@@ -9,13 +9,8 @@ namespace BMICalculator
 
             Console.WriteLine("Welcome to the BMI Calculator!");
 
-            // Get weight
-            Console.WriteLine("Enter weight in kg: ");
-            double weight = Convert.ToDouble(Console.ReadLine());
-
-            // Get height
-            Console.WriteLine("Enter Height in meters: ");
-            double height = Convert.ToDouble(Console.ReadLine());
+            double weight = GetValidInput = ("Enter weight in kilogram");
+            double height = GetValidInput = ("Enter height in meters");
 
             // Calculate BMI 
             double bmi = weight / (height * height);
@@ -39,6 +34,21 @@ namespace BMICalculator
             // Display result
             Console.WriteLine("Your BMI is: {bmi:F2}. You are classified as: {category} ");
             
+        }
+
+        static double GetValidInput(string prompt)
+        {
+            double value;
+            while (true)
+            {
+                Console.Write(prompt);
+                if (double.TryParse(Console.ReadLine(), out value) && value > 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid input. Please enter a positive number.");
+            }
+            return value;
         }
 
     }
